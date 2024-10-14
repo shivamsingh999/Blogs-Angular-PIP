c61ff5014c3a65d6f0de7879555534aabe10205f
Shivam Singh <shivam.singh@1rivet.com>
2024-10-14 19:34:38 +0530

    Update: Added Comment Section in Blogs Details

--- src/app/blog-detail/blog-detail.component.ts
+++ src/app/blog-detail/blog-detail.component.ts
@@ -1,17 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-blog-detail',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './blog-detail.component.html',
   styleUrl: './blog-detail.component.scss'
 })
 
 export class BlogDetailComponent implements OnInit {
   blog: any;
+  comments: any[] = [];
 
   constructor(private route: ActivatedRoute, private http: HttpClient) {}
 
@@ -19,6 +21,7 @@ export class BlogDetailComponent implements OnInit {
     const id = this.route.snapshot.paramMap.get('id');
     this.http.get<any>(`http://localhost:3000/blogs/${id}`).subscribe((data) => {
       this.blog = data;
+      this.comments = data.comments || []; // Fetch comments from the blog
     });
   }
 }
